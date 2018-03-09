8fe6ece9d7c80b92f575573f3261188ea2d4c821
Berenice Corín <spy.zelda@gmail.com>
2018-03-09 14:26:53 -0600

    'TutorialWPF'

---
+++ WPFTutorial/WPFTutorial/Common_interface_controls/MenuIconCheckableSample.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFTutorial.Common_interface_controls
+{
+    /// <summary>
+    /// Lógica de interacción para MenuIconCheckableSample.xaml
+    /// </summary>
+    public partial class MenuIconCheckableSample : Window
+    {
+        public MenuIconCheckableSample()
+        {
+            InitializeComponent();
+        }
+        private void mnuNew_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("New");
+        }
+    }
+}
---
+++ WPFTutorial/WPFTutorial/Rich_text_controls/FlowDocumentSearchSample.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFTutorial.Rich_text_controls
+{
+    /// <summary>
+    /// Lógica de interacción para FlowDocumentSearchSample.xaml
+    /// </summary>
+    public partial class FlowDocumentSearchSample : Window
+    {
+        public FlowDocumentSearchSample()
+        {
+            InitializeComponent();
+        }
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            fdViewer.Find();
+        }
+    }
+}
